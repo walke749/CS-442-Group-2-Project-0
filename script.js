68cb2b3cc2c04036beb32e65605ddf952335654c
walke749 <walke749@pnw.edu>
2025-08-29 15:26:58 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Wait until the DOM is ready
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("action-btn");
+
+  button.addEventListener("click", () => {
+    alert("Button clicked! 🚀");
+  });
+});
